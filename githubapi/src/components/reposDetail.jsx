d13f179bcb12c30fc91e6ffc7758de98ce67927d
victorSsantos <victor.ssantos089@gmail.com>
2020-02-05 21:16:35 -0300

    Ordenação repositorios pelo numero de estrelas

--- githubapi/src/components/reposDetail.jsx
+++ githubapi/src/components/reposDetail.jsx
@@ -4,18 +4,22 @@ import './reposDetail.css';
 export default class ReposDetail extends Component {
     render(){
         const items = [];
-
+        
         if(this.props.reposContent < 1 ){
             return <div></div>
         }else{
-            for (var i = 0 ; i <= this.props.reposContent.length -1 ; i++ ){
+            const reposContent = this.props.reposContent.sort(function(a,b) {
+                return a.stargazers_count > b.stargazers_count ? -1 : a.stargazers_count > b.stargazers_count ? 1 : 0;
+            }); ;
+
+            for (var i = 0 ; i <= reposContent.length -1 ; i++ ){
                 items.push(
-                    <div key={this.props.reposContent[i].id} className="Span-Repos-Detail">
-                        <span className="repo-name">{this.props.reposContent[i].name}</span>
-                        <span className="repo-description">{this.props.reposContent[i].description}</span>
+                    <div key={reposContent[i].id} className="Span-Repos-Detail">
+                        <span className="repo-name">{reposContent[i].name}</span>
+                        <span className="repo-description">{reposContent[i].description}</span>
                         <span className="Align-Helper">
                             <img src={require("../img/star_icon.svg")} alt="star_icon.svg"/>
-                            <span className="star-count">{this.props.reposContent[i].stargazers_count}</span>
+                            <span className="star-count">{reposContent[i].stargazers_count}</span>
                         </span>
                     </div>
                 )
